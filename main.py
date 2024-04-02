21e29a5f604aff7c8bed6c361764f19428ea218c
Radoslav Vlcek <radoslav.vlcek@gmail.com>
2024-04-02 16:00:10 +0200

    Added if __name__ == '__main__' condition

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-import init
-import os
-from window import Window
+if __name__ == "__main__":
+    import init
+    import os
+    from window import Window
 
-# Initiate main window
-window_obj = Window()
+    # Initiate main window
+    window_obj = Window()
 
-# If DB file exists, run the main window, otherwise initial window
-if os.path.isfile(window_obj.db_file):
-    init.display_main_window(window_obj)
-else:
-    init.display_init_window(window_obj)
+    # If DB file exists, run the main window, otherwise initial window
+    if os.path.isfile(window_obj.db_file):
+        init.display_main_window(window_obj)
+    else:
+        init.display_init_window(window_obj)
 
-# Process window
-window_obj.display_window()
+    # Process window
+    window_obj.display_window()
